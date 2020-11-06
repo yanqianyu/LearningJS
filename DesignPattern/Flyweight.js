d46a1623e3c97727da78810d37352ddd34f8f372
YanQianyu <284989344@qq.com>
2020-11-06 19:02:09 +0800

    add Flyweights

---
+++ DesignPattern/Flyweight.js
@@ -0,0 +1,69 @@
+class Flyweight {
+    constructor(sharedState) {
+        this.sharedState = sharedState;
+    }
+    operation(uniqueState) {
+        const s = JSON.stringify(this.sharedState);
+        const u = JSON.stringify(uniqueState);
+        console.log(`Flyweight: Displaying shared (${s}) and unique (${u}) state.`);
+    }
+}
+
+class FlyweightFactory {
+    constructor(initialFlyweights) {
+        this.flyweights = [];
+        for (const state of initialFlyweights) {
+            this.flyweights[this.getKey(state)] = new Flyweight(state);
+        }
+    }
+    
+    getKey(state) {
+        return state.join('_');
+    }
+
+    getFlyweight(sharedState) {
+        const key = this.getKey(sharedState);
+
+        if (!(key in this.flyweights)) {
+            console.log('FlyweightFactory: Can\'t find a flyweight, creating new one.');
+            this.flyweights[key] = new Flyweight(sharedState);
+        } else {
+            console.log('FlyweightFactory: Reusing existing flyweight.');
+        }
+
+        return this.flyweights[key];
+    }
+
+    listFlyweights() {
+        const count = Object.keys(this.flyweights).length;
+        console.log(`\nFlyweightFactory: I have ${count} flyweights:`);
+        for (const key in this.flyweights) {
+            console.log(key);
+        }
+    }
+}
+
+const factory = new FlyweightFactory([
+    ['Chevrolet', 'Camaro2018', 'pink'],
+    ['Mercedes Benz', 'C300', 'black'],
+    ['Mercedes Benz', 'C500', 'red'],
+    ['BMW', 'M5', 'red'],
+    ['BMW', 'X6', 'white'],
+]);
+factory.listFlyweights();
+
+
+function addCarToPoliceDatabase(ff, plates, owner, brand, model, color) {
+    console.log('\nClient: Adding a car to database.');
+    const flyweight = ff.getFlyweight([brand, model, color]);
+
+    // The client code either stores or calculates extrinsic state and passes it
+    // to the flyweight's methods.
+    flyweight.operation([plates, owner]);
+}
+
+addCarToPoliceDatabase(factory, 'CL234IR', 'James Doe', 'BMW', 'M5', 'red');
+
+addCarToPoliceDatabase(factory, 'CL234IR', 'James Doe', 'BMW', 'X1', 'red');
+
+factory.listFlyweights();
