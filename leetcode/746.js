05def0d47e01047e0eb13db5e5a76bca100a7737
YanQianyu <284989344@qq.com>
2020-12-21 08:53:43 +0800

    add leetcode 746

--- leetcode/746.js
+++ leetcode/746.js
@@ -6,7 +6,7 @@ var minCostClimbingStairs = function(cost) {
     let dp = new Array(cost.length + 1);
     dp[0] = 0;
     dp[1] = cost[0];
-    for(let i = 2; i < cost.length; i++) {
+    for(let i = 2; i <= cost.length; i++) {
         dp[i] = cost[i - 1] + Math.min(dp[i-1], dp[i-2]);
     }
 
