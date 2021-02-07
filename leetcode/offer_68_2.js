3f1c3aa89d38d4d298fd24f3aaba985a6a0dd5b6
YanQianyu <284989344@qq.com>
2021-02-07 21:28:31 +0800

    add offer 68_2

---
+++ leetcode/offer_68_2.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if (root === null) {
+        return null;
+    }
+
+    if (p === root || q === root) {
+        return root;
+    }
+
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+    if (left !== null && right !== null) {
+        return root;
+    }
+    return left === null ? right: left;
+};
