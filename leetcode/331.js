002b9c99915112d68463a20521da08810428abaa
YanQianyu <284989344@qq.com>
2021-03-12 09:28:15 +0800

    leetcode 331

---
+++ leetcode/331.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} preorder
+ * @return {boolean}
+ */
+ var isValidSerialization = function(preorder) {
+     const n = preorder.length;
+     let i = 0;
+     const stack = [1];
+     while(i < n) {
+         if (!stack.length) {
+             return false;
+         }
+         if (preorder[i] === ',') {
+             i++;
+         }
+         else if (preorder[i] === '#') {
+             stack[stack.length - 1] -= 1;
+             if (stack[stack.length - 1] === 0) {
+                 stack.pop();
+             }
+             i++;
+         }
+         else {
+             // 读一个数字
+            while (i < n && preorder[i] !== ',') {
+                ++i;
+            }
+            stack[stack.length - 1]--;
+            if (stack[stack.length - 1] === 0) {
+                stack.pop();
+            }
+            stack.push(2);
+         }
+     }
+     return stack.length === 0;
+};
