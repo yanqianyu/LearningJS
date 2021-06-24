32607b2f34e8fd43bc92518b1631f02958c19bab
YanQianyu <284989344@qq.com>
2021-06-24 09:08:49 +0800

    leetcode 149 & offer 39

---
+++ leetcode/149.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+ var maxPoints = function(points) {
+     const n = points.length;
+     if (n <= 2) {
+         return n;
+     }
+     let ret = 0;
+     for(let i = 0; i < n; i++) {
+         if (ret >= n - i || ret > n / 2) {
+             break;
+         }
+         const map = new Map();
+         for(let j = i + 1; j < n; j++) {
+             let x = points[i][0] - points[j][0];
+             let y = points[i][1] - points[j][1];
+             if (x === 0) {
+                 y = 1;
+             }
+             else if (y === 0) {
+                 x = 1;
+             }
+             else {
+                 if (y < 0) {
+                     x = -x;
+                     y = -y;
+                 }
+                 const gcdXY = gcd(Math.abs(x), Math.abs(y));
+                 x /= gcdXY;
+                 y /= gcdXY;
+             }
+             const key = y + x * 20001;
+             map.set(key, (map.get(key) || 0) + 1);
+         }
+         let maxn = 0;
+         for(const num of map.values()) {
+             maxn = Math.max(maxn, num + 1);
+         }
+         ret = Math.max(ret, maxn);
+     }
+     return ret;
+};
+
+const gcd = (a, b) => {
+    return b != 0 ? gcd(b, a % b) : a;
+}
