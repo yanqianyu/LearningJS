42168b9a886dce7732464cba2bb367f0ec196bf5
YanQianyu <284989344@qq.com>
2021-07-11 11:04:17 +0800

    leetcode 274

---
+++ leetcode/274.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function(citations) {
+    const check = (h) => {
+        let ans = 0;
+        for(let i of citations) {
+            if (i >= h) {
+                ans += 1;
+            }
+        }
+        return ans >= h;
+    }
+
+    let low = 0, high = citations.length;
+    while(low < high) {
+        let mid = (low + high) >> 1;
+        if (check(mid)) {
+            low = mid + 1;
+        }
+        else {
+            high = mid;
+        }
+    }
+    if (check(low)) {
+        return low;
+    }
+    else {
+        return low - 1;
+    }
+};
