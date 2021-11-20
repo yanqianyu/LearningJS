2c3082d96b179c1be47ee29278e73c0e942cd670
YanQianyu <284989344@qq.com>
2021-11-20 13:36:59 +0800

    lc 594

---
+++ leetcode/594.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findLHS = function(nums) {
+     nums.sort((a, b) => a - b);
+     let begin = 0, res = 0;;
+     for(let end = 0; end < nums.length; end++) {
+         while(nums[end] - nums[begin] > 1) {
+             begin += 1;
+         }
+         if (nums[end] - nums[begin] === 1) {
+             res = Math.max(res, end - begin + 1);
+         }
+     }
+     return res;
+};
